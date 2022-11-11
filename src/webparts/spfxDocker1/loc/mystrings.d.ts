953b69e4788fe8c341f9e05f9161cd6c93b74ff3
Flaviu C <flaviuc@microsoft.com>
2022-11-11 17:45:26 +0200

    initial checkin

---
+++ src/webparts/spfxDocker1/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpfxDocker1WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpfxDocker1WebPartStrings' {
+  const strings: ISpfxDocker1WebPartStrings;
+  export = strings;
+}
